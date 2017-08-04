32d108c7df24bc342b145f1940e5e40837dc35c8
alxisk <alexander.iskovskikh@gmail.com>
2017-08-04 22:38:42 +0300

    add actions related to tracks

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import * as actionTypes from '../constants/actionTypes';
+
+export const requestTracks = query => (
+  {
+    type: actionTypes.REQUEST_TRACKS,
+    query,
+  }
+);
+
+export const setNowPlaying = track => (
+  {
+    type: actionTypes.SET_NOW_PLAYING,
+    track,
+  }
+);
